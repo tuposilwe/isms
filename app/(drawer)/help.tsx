65f427e7e6e9a9069cc78c07efda631cc6a390c0
tuposilwe <rudigermkondya@gmail.com>
2025-03-31 12:44:39 +0300

    Added items in help screen

--- app/(drawer)/help.tsx
+++ app/(drawer)/help.tsx
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const help = () => {
   return (
-    <View>
-      <Text>help</Text>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text
+        style={{
+          fontWeight: "bold",
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: "30%",
+          padding: 20,
+          fontSize: 29,
+        }}
+      >
+        For Support
+      </Text>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 24,
+        }}
+      >
+        <View style={{ flexDirection: "row", alignContent: "center" }}>
+          <Ionicons name="call" size={24} color="black" />
+          <Text
+            style={{
+              fontWeight: "bold",
+              marginLeft:7
+            }}
+          >
+            0752 678 721 - IT Officer
+          </Text>
+        </View>
+        <View style={{ flexDirection: "row", alignContent: "center" }}>
+          <Ionicons name="call" size={24} color="black" />
+          <Text
+            style={{
+              fontWeight: "bold",
+              marginLeft:7
+            }}
+          >
+            0741 238 952 - IT Officer
+          </Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default help
+export default help;
