19140275553122079348875969ac8ebc5095469e
tuposilwe <rudigermkondya@gmail.com>
2025-04-01 19:13:36 +0300

    Added Noticication Badge

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,5 +1,6 @@
 import CustomDrawerContent from "@/components/CustomDrawerContent";
 import icons from "@/constants/icons";
+import useSettings from "@/hooks/useSettings";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
 import {
@@ -10,11 +11,25 @@ import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import Foundation from "@expo/vector-icons/Foundation";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 import { Drawer } from "expo-router/drawer";
-import { Image } from "react-native";
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Modal from "react-native-modal";
+
+const { width, height } = Dimensions.get("screen");
 
 const Layout = () => {
+  const { isModalVisible, toggleModal } = useSettings();
+
   return (
     <Drawer
       backBehavior="history"
@@ -54,11 +69,126 @@ const Layout = () => {
           ),
 
           headerRight: () => (
-            <Image
-              source={icons.bell}
-              className="size-8 mr-2"
-              style={{ tintColor: "#ffffff" }}
-            />
+            <>
+              <TouchableOpacity
+                style={{ position: "relative", marginRight: 10 }}
+                onPress={() => toggleModal()}
+              >
+                <Image
+                  source={icons.bell}
+                  className="size-8"
+                  style={{ tintColor: "#ffffff" }}
+                />
+                {/* Notification Badge */}
+                <View
+                  style={{
+                    position: "absolute",
+                    right: -5,
+                    top: -5,
+                    backgroundColor: "red",
+                    borderRadius: 10,
+                    width: 20,
+                    height: 20,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text style={{ color: "white", fontSize: 12 }}>1</Text>
+                </View>
+              </TouchableOpacity>
+              <Modal
+                statusBarTranslucent={true}
+                onBackdropPress={() => toggleModal()}
+                isVisible={isModalVisible}
+                onDismiss={() => toggleModal()}
+                deviceHeight={height}
+                deviceWidth={width}
+                style={{
+                  margin: 4,
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop: "100%",
+                  }}
+                >
+                  <View
+                    style={{
+                      height: "100%",
+                      width: "100%",
+
+                      backgroundColor: "white",
+                      borderTopLeftRadius: 18,
+                      borderTopRightRadius: 20,
+                      padding: 16,
+                    }}
+                  >
+                    <ScrollView>
+                      <Pressable
+                        onPress={() => toggleModal()}
+                        style={{ alignSelf: "flex-end" }}
+                      >
+                        <MaterialIcons name="cancel" size={24} color="black" />
+                      </Pressable>
+
+                      <Text
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: 20,
+                        }}
+                      >
+                        Notifications
+                      </Text>
+                      <View
+                        style={{
+                          flexDirection: "row",
+
+                          marginTop: 20,
+                          borderRadius: 10,
+                          borderWidth: 0.4,
+                          borderColor: "#978f8f",
+                          padding: 20,
+                        }}
+                      >
+                        <FontAwesome5
+                          name="brain"
+                          size={24}
+                          color="#832c1ddd"
+                        />
+                        <Text
+                          style={{
+                            fontWeight: "bold",
+                            marginLeft: 9,
+                            color: "#524e4e",
+                            fontSize: 15,
+                          }}
+                        >
+                          Deep Learning and Neural Networks Course Works Results
+                          is Out...!
+                        </Text>
+                      </View>
+                      <View style={{
+                        alignItems:"center",
+                        justifyContent:"center",
+                        marginTop:20
+                      }}>
+                        <Text
+                        style={{
+                          fontWeight: "bold",
+                          marginLeft: 9,
+                          color: "#524e4e",
+                          fontSize: 15,
+                        }}
+                        >Clear Notifications</Text>
+                      </View>
+                    </ScrollView>
+                  </View>
+                </View>
+              </Modal>
+            </>
           ),
           headerLeft: () => (
             <FontAwesome6
